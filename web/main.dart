60ac906f155c140e9846f10cac96f332bf1c2e1a
Nathan Bosch <nbosch1@gmail.com>
2016-01-23 10:35:28 -0800

    Secret Santa AngularDart app
    - NameService pulls sets of names from a json file and shuffles them
    -- People in the same set of names cannot be paired with eachother
    -- Pairs are created by ordering everyone in a single list with a
    cycle, each person gives a gift to the next person in the list
    -- Shuffling is seeded by the current year so different orders happen
    each year
    - Angular Component to show the list of names
    - Angular Component to show who recieves a gift given a gifter name
    - JSON file with sample names
    
    Future Improvements
    - Use routes so the back button works
    - Upgrade to Angular2
    - Add some style

---
+++ web/main.dart
@@ -0,0 +1,141 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:html';
+import 'dart:math';
+
+import 'package:angular/angular.dart';
+import 'package:angular/application_factory.dart';
+import 'package:di/annotations.dart';
+
+final random = new Random(new DateTime.now().year);
+
+@Injectable()
+class NameService {
+  Future<List<List<String>>> _groups;
+
+  Future<List<List<String>>> get groups => _groups ??= loadGroups();
+
+  Future<List<String>> get participants async {
+    var resolvedGroups = await groups;
+    return resolvedGroups.expand((group) => group).toList();
+  }
+
+  Future<List<List<String>>> loadGroups() async {
+    const path = '/resources/names.json';
+    var namesJson = await HttpRequest.getString(path);
+    return JSON.decode(namesJson);
+  }
+
+  Future<List<String>> _shuffledNames;
+  Future<List<String>> get shuffledNames => _shuffledNames ??= shuffleNames();
+
+  Future<List<String>> shuffleNames() async {
+    var shuffled = await participants;
+    var resolvedGroups = await groups;
+    shuffled.shuffle(random);
+    // Ensure no disallowed neighbors
+    int problemIndex = indexOfFirstBadMatch(shuffled, resolvedGroups);
+    int swapCount = 0;
+    while (problemIndex > 0) {
+      swapCount++;
+      int swapWith = random.nextInt(shuffled.length);
+      var temp = shuffled[problemIndex];
+      shuffled[problemIndex] = shuffled[swapWith];
+      shuffled[swapWith] = temp;
+      problemIndex = indexOfFirstBadMatch(shuffled, resolvedGroups);
+    }
+    return shuffled;
+  }
+
+  int indexOfFirstBadMatch(
+      List<String> participants, List<List<String>> groups) {
+    for (int i = 0; i < participants.length; i++) {
+      var participant = participants[i];
+      var group = groups.firstWhere((group) => group.contains(participant));
+      var matchIndex = i + 1 >= participants.length ? 0 : i + 1;
+      if (group.contains(participants[matchIndex])) {
+        return matchIndex;
+      }
+    }
+    return -1;
+  }
+}
+
+@Component(
+    selector: 'secret-santa',
+    template: '''
+  <name-list name="name" names="names" ng-if="!nameChosen">
+  </name-list>
+  <giftee ng-if="nameChosen" from="name">
+  </giftee>
+''')
+class SecretSanta {
+  String name;
+  bool get nameChosen => name != null;
+  List<String> names;
+
+  SecretSanta(NameService nameService) {
+    nameService.participants.then((participants) {
+      names = participants;
+    });
+  }
+}
+
+@Component(
+    selector: 'name-list',
+    template: '''
+Who are you? (be honest!)
+<p ng-repeat="name in names"><a href="" ng-click="choose(name)">{{name}}</a></p>''')
+class NameList {
+  @NgOneWay('names')
+  List<String> names;
+
+  @NgTwoWay('name')
+  String name;
+
+  choose(String choice) {
+    name = choice;
+  }
+}
+
+@Component(
+    selector: 'giftee',
+    template: '''Hey {{from}}, You should get a gift for {{giftee}}''')
+class Giftee {
+  var _fromCompleter = new Completer<String>();
+
+  @NgOneWay('from')
+  void set from(String from) {
+    _fromCompleter.complete(from);
+  }
+
+  String _from;
+
+  String get from => _from;
+
+  String giftee;
+
+  Giftee(NameService nameService) {
+    Future.wait([_fromCompleter.future, nameService.shuffledNames])
+        .then((results) {
+      _from = results[0];
+      var shuffled = results[1];
+      var giverIndex = shuffled.indexOf(from);
+      var next = giverIndex + 1 >= shuffled.length ? 0 : giverIndex + 1;
+      giftee = shuffled[next];
+    });
+  }
+}
+
+class SecretSantaModule extends Module {
+  SecretSantaModule() {
+    bind(NameList);
+    bind(SecretSanta);
+    bind(Giftee);
+    bind(NameService);
+  }
+}
+
+void main() {
+  applicationFactory().addModule(new SecretSantaModule()).run();
+}
